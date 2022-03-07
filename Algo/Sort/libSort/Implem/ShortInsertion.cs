68ae51f08b7b01717b6614114cb342470521c92f
machard <pascal.machard@gmail.com>
2022-03-07 21:14:52 +0100

    refactoring

--- Algo/Sort/libSort/Implem/ShortInsertion.cs
+++ Algo/Sort/libSort/Implem/ShortInsertion.cs
@@ -10,9 +10,24 @@ namespace Utopia.Algo.Sort.Implem
             return SortInsert(tabSource.Clone() as int[]);
         }
 
-        private static int[] SortInsert(int[] a)
+        private static int[] SortInsert(int[] tab)
         {
-            return a;
+            int[] result = new int[tab.Length];
+            int x,j;
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                x = tab[i];
+                j = i;
+
+                while ((j > 0) && (tab[j - 1] > x))
+                {
+                    tab[j] = tab[j - 1];
+                    j = j - 1;
+                    tab[j] = x;
+                }
+            }
+            return result;
         }
     }
 }
